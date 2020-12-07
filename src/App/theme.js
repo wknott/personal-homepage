f58d144e0628d3330597fe9757417ca22fae9384
Wojciech Knott <wojciechknott@gmail.com>
2020-12-07 18:16:06 +0100

    added theme files with light and dark theme

---
+++ src/App/theme.js
@@ -0,0 +1,23 @@
+export const lightTheme = {
+  color: {
+    mainBlue: "#0366D6",
+    body: "#E5E5E5",
+    text: "#252525",
+    secondaryText: "6E7E91",
+    toggleBorder: "#FFF",
+    background: "#363537",
+    white: "#FFFFFF",
+  },
+}
+
+export const darkTheme = {
+  color: {
+    mainBlue: "#2188FF",
+    body: "#252525",
+    text: "#FFFFFF",
+    secondaryText: "FFF",
+    toggleBorder: "#FFF",
+    background: "#363537",
+    white: "#FFFFFF",
+  },
+}

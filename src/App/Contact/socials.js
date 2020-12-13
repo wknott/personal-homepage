8cd7e10ee94da909cb6e3082c6a128a4305ad539
Wojciech Knott <wojciechknott@gmail.com>
2020-12-13 08:30:04 +0100

    created socials file with socials and email objects

---
+++ src/App/Contact/socials.js
@@ -0,0 +1,23 @@
+import githubIcon from "./Github_black.svg";
+import facebookIcon from "./Facebook_black.svg";
+import linkedinIcon from "./LinkedIN_black.svg";
+
+export const email = "wojciechknott@gmail.com";
+
+export const socials = [
+  {
+    id: "github",
+    imgUrl: githubIcon,
+    url: "https://github.com/wknott",
+  },
+  {
+    id: "facebook",
+    imgUrl: facebookIcon,
+    url: "https://www.facebook.com/wojciech.knott",
+  },
+  {
+    id: "linkedin",
+    imgUrl: linkedinIcon,
+    url: "https://www.linkedin.com/in/wojciechknott/",
+  },
+];

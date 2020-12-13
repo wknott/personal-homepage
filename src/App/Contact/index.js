71f3379def393920395cf74eeca917aa2abba3fc
Wojciech Knott <wojciechknott@gmail.com>
2020-12-13 08:29:22 +0100

    added contact section component

---
+++ src/App/Contact/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Description, MiniTitle, StyledInfo, Email, SocialContainer, Link, Icon } from "./styled";
+import { email, socials } from "./socials";
+
+const Contact = () => (
+  <StyledInfo>
+    <MiniTitle>Let's talk!</MiniTitle>
+    <Email href={`mailto:${email}`}>{email}</Email>
+    <Description>
+      I’m always open to new projects whenever I have the time.
+      If you have a website, dashboard or mobile app in mind and need some help
+      to make your ideas come to life, feel free to conatct me!
+    </Description>
+    <SocialContainer>
+      {socials.map(social =>
+        <Link id={social.id} href={social.url} target="_blank">
+          <Icon src={social.imgUrl} />
+        </Link>
+      )}
+    </SocialContainer>
+  </StyledInfo>
+);
+
+export default Contact;

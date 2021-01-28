c5033d82a9961b1700ed700ff7946472b56136bd
Wojciech Knott <wojciechknott@gmail.com>
2021-01-28 21:19:31 +0100

    moved profile image to img tag, fixed misspell, changed border-radius to 50%

--- src/App/About/Image/index.js
+++ src/App/About/Image/index.js
@@ -1,31 +1,25 @@
 import styled from "styled-components";
-import profilImage from "./profil-image.png";
 
-export default styled.div`
+
+export default styled.img`
   grid-area: image;
   width: 384px;
   height: 384px;
-  border-radius: 192px;
-  background: url(${profilImage});
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
+  border-radius: 50%;
   background-color: ${({ theme }) => theme.color.background};
-  transition: all 0.5s linear; 
+  transition: background-color 0.5s linear; 
   margin-right: 66px;
   flex-shrink: 0;
 
   @media(max-width: ${({ theme }) => theme.breakpoint.iPad}px) {
     width: 256px;
     height: 256px;
-    border-radius: 128px;
     margin-right: 48px;
   };
 
   @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     width: 128px;
     height: 128px;
-    border-radius: 64px;
     margin-bottom: 16px;
     margin-top: 13px;
   };
--- src/App/About/index.js
+++ src/App/About/index.js
@@ -6,10 +6,11 @@ import { Wrapper, Icon } from "./styled";
 import ThemeButton from "./ThemeButton";
 import message from "./Message.svg";
 import { email } from "../Contact/socials";
+import profileImage from "./profile-image.png";
 
 const About = ({ theme, themeToggler }) => (
   <Wrapper>
-    <Image />
+    <Image src={profileImage} alt="my profile picture" />
     <Info
       miniTitle="This is"
       title="Wojciech Knott"

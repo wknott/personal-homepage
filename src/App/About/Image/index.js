c661cb75bcd87d44e45908328c001eb466cf9541
Wojciech Knott <wojciechknott@gmail.com>
2021-02-03 23:13:58 +0100

    changed avatar width to more responsive

--- src/App/About/Image/index.js
+++ src/App/About/Image/index.js
@@ -2,22 +2,17 @@ import styled from "styled-components";
 
 export default styled.img`
   grid-area: image;
-  width: 384px;
-  height: 384px;
+  width: 30vw;
+  max-width: 384px;
+  height: auto;
   border-radius: 50%;
   background-color: ${({ theme }) => theme.color.background};
   transition: background-color 0.5s linear; 
   margin-right: 64px;
 
-  @media(max-width: ${({ theme }) => theme.breakpoint.iPad}px) {
-    width: 256px;
-    height: 256px;
-    margin-right: 48px;
-  };
 
   @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     width: 128px;
-    height: 128px;
     margin-bottom: 16px;
     margin-top: 13px;
   };

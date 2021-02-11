bf5f22dc056cb5431be3f1b84e1654986f87f9ce
Wojciech Knott <wojciechknott@gmail.com>
2021-02-11 12:30:43 +0100

    moved dark theme logic to redux

--- src/App/About/ThemeButton/index.js
+++ src/App/About/ThemeButton/index.js
@@ -1,17 +1,24 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsDarkTheme, toggleTheme } from "../../themeSlice";
 import { Box, Button, Container, Icon, Text, IconWrapper } from "./styled";
 
-const ThemeButton = ({ theme, toggleTheme }) => (
-  <Container>
-    <Button onClick={toggleTheme}>
-      <Text>Dark mode {theme === "light" ? "off" : "on"}</Text>
-      <Box>
-        <IconWrapper moveToRight={theme === "light"}>
-          <Icon />
-        </IconWrapper>
-      </Box>
-    </Button>
-  </Container >
-);
+const ThemeButton = () => {
+  const isDarkTheme = useSelector(selectIsDarkTheme);
+  const dispatch = useDispatch();
+
+  return (
+    <Container>
+      <Button onClick={() => dispatch(toggleTheme())}>
+        <Text>Dark mode {isDarkTheme ? "on" : "off"}</Text>
+        <Box>
+          <IconWrapper moveToRight={!isDarkTheme}>
+            <Icon />
+          </IconWrapper>
+        </Box>
+      </Button>
+    </Container >
+  );
+}
 
 export default ThemeButton;
--- src/App/index.js
+++ src/App/index.js
@@ -3,27 +3,26 @@ import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { darkTheme, lightTheme } from './theme';
 import { GlobalStyle } from './GlobalStyle';
-import { useDarkMode } from "./useDarkMode"
 import About from "./About";
 import Section from "./Section";
 import { skills, futureSkills } from "./skills";
 import Portfolio from "./Portfolio";
 import Contact from "./Contact";
 import { Wrapper, Container } from "./styled";
+import { useSelector } from "react-redux";
+import { selectIsDarkTheme } from "./themeSlice";
 
 const App = () => {
-  const [themeMode, toggleTheme] = useDarkMode();
-
-  const theme = themeMode === 'light' ? lightTheme : darkTheme;
+  const isDarkTheme = useSelector(selectIsDarkTheme);
 
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={isDarkTheme ? darkTheme : lightTheme}>
       <>
         <Normalize />
         <GlobalStyle />
         <Wrapper>
           <Container>
-            <About theme={themeMode} toggleTheme={toggleTheme} />
+            <About />
             <Section header="My skillset includes 🛠️" skills={skills} />
             <Section header="What I want to learn next 🚀" skills={futureSkills} />
             <Portfolio />
